a2827cca0527112c3f6dddae88062102060d638c
risenjapanllc-rgb <risen.japan.llc@gmail.com>
2026-05-28 22:24:44 +0900

    Connect route guard to UI adapter

--- src/core/cocreative/uiAdapter.ts
+++ src/core/cocreative/uiAdapter.ts
@@ -1,8 +1,11 @@
 import { executeDream } from "./executeDream";
+import { assertCoreRoute } from "./routeGuard";
 
 export function renderDreamFromInput(
   text: string
 ): string {
+  assertCoreRoute("COCREATIVE_CORE");
+
   return executeDream({
     text,
   });
